52f18ddd67c1edfb831480bdd2fbdd5e2727bd6d
Eric <wang.yaohui@dhs.sg>
2021-02-22 16:34:17 +0800

    Edit boss tab

--- src/components/header-components/Header.js
+++ src/components/header-components/Header.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import './css/Header.css';
 // import '../../../node_modules/font-awesome/css/font-awesome.min.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import { Nav, NavLink, Bars, NavMenu, NavBtn, NavBtnLink } from 'react-bootstrap'
 
 import SearchBar from './SearchBar';
 import ValkryieDropdownMenu from './ValkyrieDropdownMenu';
