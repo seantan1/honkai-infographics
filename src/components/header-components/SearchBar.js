70b6ed76fc204e18728915cc409198ddc7514dba
pururun <sean1360851@gmail.com>
2021-02-22 15:15:05 +1100

    fixed jsx compilation issue

--- src/components/header-components/SearchBar.js
+++ src/components/header-components/SearchBar.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import './css/SearchBar.css';
 import '../../../node_modules/font-awesome/css/font-awesome.min.css';
 
--- src/components/header-components/ValkDropdownItem.js
+++ src/components/header-components/ValkDropdownItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './css/ValkDropdownItem.css';
 
 const DropdownItem = ({valkName, chibiIcon}) => {
