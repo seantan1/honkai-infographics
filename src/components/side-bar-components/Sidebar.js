ba6c74b89c046f2d371e477e20f5ffb6b479219d
pururun <sean1360851@gmail.com>
2021-02-25 13:07:47 +1100

    implementing sidebar

--- src/components/side-bar-components/Sidebar.js
+++ src/components/side-bar-components/Sidebar.js
@@ -1,11 +1,13 @@
 import './css/Sidebar.css';
 
 import SearchBar from './SearchBar';
+import SidebarContents from './SidebarContents';
 
 function Sidebar() {
     return (
         <div >
             <SearchBar />
+            <SidebarContents />
         </div>
     )
 }
