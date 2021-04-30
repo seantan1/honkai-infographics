47a803c496e144ae5d7b20686a582ffee6b0c75f
sean890 <sean1360851@gmail.com>
2021-04-30 10:32:11 +1000

    implemented routes using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 
-import logo from './logo.svg';
+
 import './App.css';
 import Header from './components/header-components/Header';
 import Banner from './components/banner-components/Banner';
 import Sidebar from './components/side-bar-components/Sidebar';
+import Main from './Main'
 
 
 
@@ -42,25 +43,7 @@ function App() {
                 <Header hamburgerClicked={hamburgerClicked} headerComponentsDisplayState={headerComponentsDisplayState}/>
                 <Banner />
 
-
-                <div className="App">
-
-                    <header className="App-header">
-
-                        <img src={logo} className="App-logo" alt="logo" />
-                        <p>
-                            Edit <code>src/App.js</code> and save to reload. 
-        </p>
-                        <a
-                            className="App-link"
-                            href="https://reactjs.org"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            Learn React
-        </a>
-                    </header>
-                </div>
+                <Main />
             </div>
 
         </div>
